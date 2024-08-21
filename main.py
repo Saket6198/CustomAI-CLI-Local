cd1bfcfe78f143c82739be7c59df85896b9ce7f6
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-08-21 19:34:43 +0530

    added ConversationBufferMemory and ConversationChain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
 import os
+
+from langchain.chains.conversation.base import ConversationChain
+from langchain.memory import ConversationBufferMemory
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.schema import (
     SystemMessage,
@@ -19,15 +22,24 @@ def main():
         SystemMessage(content="You are  a helpful assistant"),
     ]
     print("Hello, I am Your personal Assistant!")
-
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+    conversation = ConversationChain(
+        llm=llm,
+        memory=ConversationBufferMemory(),
+        verbose=True
+    )
     while True:
         user_input = input("> ")
 
-        messages.append(HumanMessage(content=user_input))
-        ai_response = chat.invoke(messages)
-        print("\nAssistant:\n", ai_response)
+        # messages.append(HumanMessage(content=user_input))
+        # ai_response = chat.invoke(messages)
+        # print("\nAssistant:\n", ai_response)
+        #
+        # messages.append(AIMessage(content=ai_response.content))
+        ai_response = conversation.predict(input=user_input)
+        messages.append(AIMessage(content=ai_reponse))
+        print(f'\nAssistant: \n{ai_response}')
 
-        messages.append(AIMessage(content=ai_response.content))
 
 if __name__ == "__main__":
     main()
