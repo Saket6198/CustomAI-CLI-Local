c5ce3a23e6c180221a499783f008ff17513e4419
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-08-21 19:09:32 +0530

    enabled basic conversation environment

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from dotenv import load_dotenv
 import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage,
+
+)
+
 
 def main():
     load_dotenv()
-    print(os.getenv("API_KEY"))
+    # print(os.getenv("GOOGLE_API_KEY"))
+
+    chat = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+    messages = [
+        SystemMessage(content="You are  a helpful assistant"),
+    ]
+    print("Hello, I am Your personal Assistant!")
+
+    while True:
+        user_input = input("> ")
+
+        messages.append(HumanMessage(content=user_input))
+        ai_response = chat.invoke(messages)
+        print("\nAssistant:\n", ai_response)
+
+        messages.append(AIMessage(content=ai_response.content))
 
 if __name__ == "__main__":
     main()
